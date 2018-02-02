7de25eb951998a175cc2aa2e35659de7632f29a9
jcornmanhomonoff <jessica.cornman-homonoff@generalassemb.ly>
2018-02-02 10:38:14 -0500

    Remove starter code
    - Remove inaccurate code from starter files for auth

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,14 +1,5 @@
 'use strict'
 
-const signUpSuccess = function (data) {
-  console.log(data)
-}
-
-const signUpFailure = function (error) {
-  console.error(error)
-}
-
 module.exports = {
-  signUpSuccess,
-  signUpFailure
+
 }
