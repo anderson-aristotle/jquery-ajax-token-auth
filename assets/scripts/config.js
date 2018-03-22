f03b7d1cb96f70fdf67e04fa321a5ade84bc98e8
tvlangley <chibia2@gmail.com>
2018-03-22 12:23:52 -0400

    Update 024/master from browser-template
    Refresh for cohort 024

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,10 +1,17 @@
 'use strict'
 
-const config = {
-  apiOrigins: {
-    production: 'https://ga-library-api.herokuapp.com',
-    development: 'https://ga-library-api.herokuapp.com'
-  }
+let apiUrl
+const apiUrls = {
+  production: '<replace-with-heroku-url>',
+  development: 'http://localhost:4741'
 }
 
-module.exports = config
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+module.exports = {
+  apiUrl
+}
