796ca2f2b2e9999adb919d426cfa5d464fe81d50
cpearce31 <cpearce31@gmail.com>
2018-08-03 11:49:37 -0400

    Remove references to URL-encoded content
    See #83 for details. Close #83.

--- curl-scripts/url-encoded/sign-up.sh
+++
@@ -1,9 +0,0 @@
-# sh curl-scripts/url-encoded/sign-up.sh
-
-curl "https://wdi-library-api.herokuapp.com/sign-up" \
-  --include \
-  --request POST \
-  --header "Content-Type: application/x-www-form-urlencoded" \
-  --data-urlencode ''
-
-echo
